b28bd2aacfbef0ff93a6b168ce094ef5b24473eb
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2024-06-04 18:14:56 -0300

    feat(tags): Creating the default tags tf module

---
+++ modules/tags/outputs.tf
@@ -0,0 +1,9 @@
+output "default_tags" {
+  description = "Represents all the tags within the project"
+  value       = local.default_tags
+}
+
+output "default_name" {
+  description = "Represents the resource's parsed name"
+  value       = local.default_name
+}
---
+++ modules/tags/variables.tf
@@ -0,0 +1,30 @@
+variable "environment" {
+  type        = string
+  description = "The resource's environment"
+}
+
+variable "application" {
+  type        = string
+  description = "The application that owns the resource"
+}
+
+variable "team" {
+  type        = string
+  description = "The squad that owns the resource"
+}
+
+variable "aws_object" {
+  type        = string
+  description = "he resource identifier, ex: ec2, s3, rds, etc..."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the resource"
+}
+
+variable "additional_tags" {
+  type        = map(any)
+  default     = {}
+  description = "A map containing additional tags"
+}
