8568a56e7004abe7117e2f96da78c773d277053d
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2024-06-04 18:37:16 -0300

    feat(s3): Implementing versioning configuration

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,8 +1,38 @@
+module "tags" {
+  source = "git@github.com:luizandrends/terraform-aws-modules.git//modules/tags?ref=v1.1.0"
+
+  name            = var.name
+  environment     = var.environment
+  application     = var.application
+  team            = var.team
+  aws_object      = "s3"
+  additional_tags = var.additional_tags
+}
+
+resource "random_string" "this" {
+  length    = 7
+  special   = false
+  lower     = true
+  min_lower = 7
+}
+
 resource "aws_s3_bucket" "this" {
-  count  = var.create ? 1 : 0
-  bucket = var.name
+  count = var.create_bucket ? 1 : 0
+
+  bucket = "${module.tags.default_name}-${random_string.this.result}"
 
   force_destroy       = var.force_destroy
   object_lock_enabled = var.object_lock_enabled
-  tags                = var.tags
+  tags                = module.tags.default_tags
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  count = var.create_bucket && length(keys(var.versioning)) > 0 ? 1 : 0
+
+  bucket = aws_s3_bucket.this[0].id
+
+  versioning_configuration {
+    # Valid values: "Enabled" or "Suspended"
+    status = try(var.versioning["enabled"] ? "Enabled" : "Suspended", tobool(var.versioning["status"]) ? "Enabled" : "Suspended", title(lower(var.versioning["status"])))
+  }
 }
